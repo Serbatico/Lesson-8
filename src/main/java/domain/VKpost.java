274816bc7c5b72a2378b8ea601e5f310ec3c5fbb
Serbatico <Serbatico@yandex.ru>
2021-10-19 15:26:35 +0300

    0.0.1

---
+++ src/main/java/domain/VKpost.java
@@ -0,0 +1,12 @@
+package domain;
+
+public class VKpost {
+    private String publicName;
+    private String time;
+    private String description;
+    private String postLink;
+    private String likes;
+    private String reposts;
+    private String views;
+
+}
